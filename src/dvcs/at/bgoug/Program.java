34b38e70009a5784f50b88b6b4c4c34007f0ae1e
Mihail Stoynov <mihail@stoynov.com>
2011-05-11 12:13:13 +0300

    Initial commit

---
+++ src/dvcs/at/bgoug/Program.java
@@ -0,0 +1,8 @@
+package dvcs.at.bgoug;
+
+public class Program {
+	public static void main(String... args) {
+		System.out.println("This presentation is interesting!");
+		System.out.println("Don't fall asleep!");
+	}
+}
