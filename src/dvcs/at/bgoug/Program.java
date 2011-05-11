2ce9351dbd291092415f936816e2d17fba1256a6
Mihail Stoynov <mihail@stoynov.com>
2011-05-11 17:49:00 +0300

    javadoc added

--- src/dvcs/at/bgoug/Program.java
+++ src/dvcs/at/bgoug/Program.java
@@ -1,5 +1,10 @@
 package dvcs.at.bgoug;
 
+/**
+ * Javadoc added
+ * @author c00l
+ *
+ */
 public class Program {
 	public static void main(String... args) {
 		System.out.println("This presentation is interesting!");
