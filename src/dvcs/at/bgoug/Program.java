b757cb6aeab9a817c5e2dd71e882aacdd6f86ef3
Mihail Stoynov <mihail@stoynov.com>
2011-06-15 16:54:22 +0300

    another syso

--- src/dvcs/at/bgoug/Program.java
+++ src/dvcs/at/bgoug/Program.java
@@ -9,6 +9,7 @@ public class Program {
 	public static void main(String... args) {
 		System.out.println("This presentation is interesting!");
 		System.out.println("Don't fall asleep!");
+		System.out.println("if you do, try to do it quietly!");
 	}
 	
 	public void method2() {}
